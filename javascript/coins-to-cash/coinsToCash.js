a2b95dfe11f5d1b4ede613a4ca660b646eaf438f
Chase Steely <chasesteely@gmail.com>
2017-10-11 10:06:01 -0500

    Completed coinsToCash exercise

---
+++ javascript/coins-to-cash/coinsToCash.js
@@ -0,0 +1,29 @@
+const piggyBank = { 
+    quarters: 5,
+    dimes: 2,
+    nickles: 5,
+    pennies: 342 
+}
+
+let dollarAmount = 0;
+
+console.log(Object.values(piggyBank))
+
+for (let key in piggyBank) {
+    if (piggyBank.hasOwnProperty(key)) {
+        let element = piggyBank[key];
+        console.log(element)
+    }
+}
+
+dollarAmount = piggyBank.pennies/100;
+console.log('$' + dollarAmount.toFixed(2))
+
+dollarAmount = piggyBank.quarters/4;
+console.log('$' + dollarAmount.toFixed(2))
+
+dollarAmount = piggyBank.dimes/10;
+console.log('$' + dollarAmount.toFixed(2))
+
+dollarAmount = piggyBank.nickles/20;
+console.log('$' + dollarAmount.toFixed(2))
